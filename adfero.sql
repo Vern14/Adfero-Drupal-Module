cf4d4ecea9ffa3f2c713b8acc4e5ed8582161447
vern14 <vzidana@gmail.com>
2015-01-28 22:57:48 -0800

    fourth

--- adfero.sql
+++ adfero.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 29, 2015 at 04:43 AM
+-- Generation Time: Jan 29, 2015 at 07:54 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -32,15 +32,19 @@ CREATE TABLE IF NOT EXISTS `adfero` (
   `lastname` text NOT NULL,
   `phone` varchar(50) NOT NULL,
   `email` varchar(25) NOT NULL,
+  PRIMARY KEY (`adferoid`),
   UNIQUE KEY `adferoid` (`adferoid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `adfero`
 --
 
 INSERT INTO `adfero` (`adferoid`, `firstname`, `lastname`, `phone`, `email`) VALUES
-(10, 'Vernon', 'Zidana', '3303474180', 'vzidana14@wooster.edu');
+(1, 'vernon', 'zidana', '3303474180', 'vzidana14@wooster.edu'),
+(2, 'Chifundo ', 'Zidana', '3303474180', 'chifundozidana@gmail.com'),
+(3, 'Joshua', 'Zidana', '3303474180', 'jzidana@gmail.com'),
+(4, 'Chosa', 'Zidana', '3303474180', 'Czidana@gmail.com');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
